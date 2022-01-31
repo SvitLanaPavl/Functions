d38f6232a2681a6ba0adb007fc529d8a03f71034
Svitlana Pavlovska <97898647+SvitLanaPavl@users.noreply.github.com>
2022-01-31 14:56:17 +0200

    Delete portfolio directory

--- portfolio/js/interactivity.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const navbarLinks = document.getElementsByClassName('nav')[0];
-const flexDisplay = document.getElementsByClassName
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
