414451cf4081e03f71f3e341b4b8b5edf7ac6781
Svitlana <lanapavlovska90@gmail.com>
2022-02-02 16:44:58 +0200

    more tasks completed

--- practices/practices.js
+++ practices/practices.js
@@ -64,19 +64,54 @@ let result = document.getElementById('stringResult');
 function animateString () {
 
 let text = string.value;
-    setInterval (function() {
+ setInterval (function() {
         text = text[text.length-1] + text.substring(0, text.length - 1);
 result.innerHTML = text; 
     }, 1000);
-    
-}
-
-function clear () {
-
-    string = '';
-    result = '';
    
 };
-console.log(clear());
+
+/*const clear = () => {
+    
+    clearInterval(stringOutput);
+    }
+    clear();*/  //not sure how I can clear the previously input value and the animated string
+
+//Finding the leap year 
+    const leapYear = () => {
+        let year = document.getElementById('year');
+        let lyear = document. getElementById('lonyear');
+        if (year.value % 4 === 0) {
+            if (year.value % 100 !== 0) {
+                return lyear.innerHTML = 'It\'s a leap year';
+            }
+            return lyear.innerHTML = 'It\'s a leap year'; 
+        }
+        else {
+            return lyear.innerHTML = 'It\'s not a leap year';
+        }
+    }
+    
+// Find whether January 1 is a Sunday in the years 2014 - 2050
+
+for (let year = 2014; year <= 2050; year ++) {
+    let d = new Date (year, 0, 1); //year, month index, day
+    if (d.getDay() === 0) {
+        console.log(`January 1 is Sunday in the year ${year}.`)
+    }
+}
+
+//number guess game
+const guessGame = () => {
+    let randNum = Math.floor(Math.random()*10);
+    let userNum = document.getElementById('number');
+    let output = document.getElementById('output');
+    if (randNum === userNum.value) {
+        output.innerHTML = 'Good work!'
+    }
+    else {
+        output.innerHTML = 'Sorry! Not matched!'
+    }
+}
 
 
